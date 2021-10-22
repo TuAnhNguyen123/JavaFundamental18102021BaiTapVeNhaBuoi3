4073a48764edcb63e8b3760b3740c16e75a8adc7
JoseNguyen <abc@example.com>
2021-10-22 19:28:00 +0700

    Initial commit

---
+++ src/Buoi1.java
@@ -0,0 +1,13 @@
+public class Buoi1 {
+    public static void main(String[] args) {
+//        String name = "Nguyễn Anh Tú";
+//        int age = 24;
+//        String address = "Quận 1";
+//        float height = 1.75f;
+//        boolean isMale = true;
+//        System.out.print("Tên: " + name + " Tuổi: "+ age + " Chiều cao: "+ height+ " Giới tính: "+ isMale);
+//       System.out.println(name +"\n"+ age+"\n"+address+"\n"+isMale +"\n" + height );
+        
+        }
+    }
+
