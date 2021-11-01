8e36d00499118a1a5e9aea106deacc174f626758
JoseNguyen <abc@example.com>
2021-11-01 18:03:35 +0700

    contructor

--- src/Buoi1.java
+++ src/Buoi1.java
@@ -1,14 +1,18 @@
-import java.util.function.IntToDoubleFunction;
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
 
 public class Buoi1 {
     public static void main(String[] args) {
+       /* {
 //        String name = "Nguyễn Anh Tú";
 //        int age = 24;
 //        String address = "Quận 1";
 //        float height = 1.75f;
 //        boolean isMale = true;
 //        System.out.print("Tên: " + name + " Tuổi: "+ age + " Chiều cao: "+ height+ " Giới tính: "+ isMale);
-//       System.out.println(name +"\n"+ age+"\n"+address+"\n"+isMale +"\n" + height );
+//       System.out.println(name +"\n"+ age+"\n"+address+"\n"+isMale +"\n" + height );}*/
+        //Toán tử
         /*int a = 5;
         int b=6;
         int ketQua = a++ - --b + --b - a++ + b-- -a-- - b++ - --a;
@@ -29,6 +33,7 @@ public class Buoi1 {
         int ketQua2 = ++a - --b - --a -b-- + a-- +b-- - b++;*/
         //             6 - 5  - 5 - 5 + 5 + 4 - 3
         //b = 4; a = 4
+        //Do you 18+
        /* int age = 18;
         if(age <18){
             System.out.println("Dưới vị thành niên");
@@ -45,6 +50,7 @@ public class Buoi1 {
         }else{
             System.out.println(-1);
         }*/
+        //Tax
       /*  int total = 100000;
         if (total>0) {
             if (total > 100000) {
@@ -63,6 +69,7 @@ public class Buoi1 {
             System.out.println("Error!!");
         }
     }*/
+        /*
         String [] arrNames = {"Teo","Ti","Tun"};
         System.out.println(arrNames[0]);
         System.out.println(arrNames.length);
@@ -71,9 +78,70 @@ public class Buoi1 {
         // vòng lặp
         for(int i =0; i< arrNames.length;i++){
             System.out.println(i);
+        }*/
+      /* for(int i = 2; i<=100;i+=2){
+             System.out.println(i);
+            }*/
+          //Chi in ra so le
+      /*for(int i = 1; i<=100;i+=2){
+            System.out.println(i);
         }
+        // Chi in ra so chia 3 du 1
+        for(int i = 1; i<=100;i+=3){
+            System.out.println(i);
+        }*/
+        //BMI
+      /*  {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your height: ");
+        float height = scanner.nextInt();
+        System.out.println("Enter your weight: ");
+        float weight = scanner.nextInt();
+        if (ChieuCao <= 0 && CanNang <= 0) {
+            System.out.println("Error!!! ");
+        } else {
+            float BMI = CanNang / (ChieuCao * ChieuCao);
+            System.out.println("BMI của bạn là : " + BMI);
+            if (BMI < 18.5) {
+                System.out.println("Thiếu cân");
+            } else if (BMI > 25) {
+                System.out.println("Thừa cân");
+            } else {
+                System.out.println("Bình thường");
+            }
+        }
+        scanner.close();
+    }*/
+        //Kiễm tra số nguyên tố
+       /* {
+            int temp = 0;
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter number: ");
+            int number = scanner.nextInt();
+            if (number < 2) {
+                System.out.println("Your number is not Prime!! ");
+            } else {
+                for (int i = 2; i <= sqrt(number); i++) {
+                    if (number % i == 0) {
+                        temp++;
+                    }
+                }
+                if (temp == 0) {
+                    System.out.println("Your number is Prime!! ");
+                } else {
+                    System.out.println("Your number is not Prime!! ");
+                }
+            }
+        }*/
+       /* int total = Sum(5,10);
+        System.out.println(total);*/
     }
+       /* public static int Sum(int a, int b){
+            return a+b;
+        }*/
 
 
-}
+        }
+
+
 
